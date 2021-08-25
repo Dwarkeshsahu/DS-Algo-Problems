35189e3ed27d66973260f437be5249b90655879c
Dwarika Teli <dwarkeshsahu@gmail.com>
2021-08-25 22:48:45 +0530

    Update Count_1’s_in_a_sorted_binary_array.py

--- Count 1’s in a sorted binary array/Count_1’s_in_a_sorted_binary_array.py
+++ Count 1’s in a sorted binary array/Count_1’s_in_a_sorted_binary_array.py
@@ -16,7 +16,24 @@ Output: 0
 
 """
 
+# Code 1
+class Solution:
+    def countOnes(self,arr, N):
+        left = 0
+        right = N-1
+        while left<= right:
+            mid = (left+right)//2
+            if arr[mid] == 0:
+                right = mid-1
+            else:
+                if mid == right or arr[mid+1] == 0:
+                    return mid+1
+                else:
+                    left = mid+1
+        return 0
+        
 
+#  code with improvement for early stopping
 
 class Solution:
     def countOnes(self,arr, N):

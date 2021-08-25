98955b979601b17a45e6a69274ca4983180fb344
Dwarika Teli <dwarkeshsahu@gmail.com>
2021-08-25 22:47:08 +0530

    Create Count_1’s_in_a_sorted_binary_array.py

---
+++ Count 1’s in a sorted binary array/Count_1’s_in_a_sorted_binary_array.py
@@ -0,0 +1,39 @@
+"""
+https://www.geeksforgeeks.org/count-1s-sorted-binary-array/
+
+Given a binary array sorted in non-increasing order, count the number of 1’s in it. 
+
+Examples: 
+
+Input: arr[] = {1, 1, 0, 0, 0, 0, 0}
+Output: 2
+
+Input: arr[] = {1, 1, 1, 1, 1, 1, 1}
+Output: 7
+
+Input: arr[] = {0, 0, 0, 0, 0, 0, 0}
+Output: 0
+
+"""
+
+
+
+class Solution:
+    def countOnes(self,arr, N):
+        left = 0
+        right = N-1
+        while left<= right:
+            mid = (left+right)//2
+            
+            if arr[mid] == 0:
+                if left == mid or arr[mid-1] == 1:
+                    return mid
+                else:
+                    right = mid-1
+            else:
+                if mid == right or arr[mid+1] == 0:
+                    return mid+1
+                else:
+                    left = mid+1
+        return 0
+            

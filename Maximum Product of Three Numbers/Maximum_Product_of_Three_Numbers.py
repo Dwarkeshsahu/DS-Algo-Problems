98c3804c72230794ef87097084a8838bbb31ec04
Dwarika Teli <dwarkeshsahu@gmail.com>
2021-09-06 23:04:14 +0530

    Create Maximum_Product_of_Three_Numbers.py

---
+++ Maximum Product of Three Numbers/Maximum_Product_of_Three_Numbers.py
@@ -0,0 +1,47 @@
+"""
+Given an integer array nums, find three numbers whose product is maximum and return the maximum product.
+
+ 
+
+Example 1:
+
+Input: nums = [1,2,3]
+Output: 6
+Example 2:
+
+Input: nums = [1,2,3,4]
+Output: 24
+Example 3:
+
+Input: nums = [-1,-2,-3]
+Output: -6
+ 
+
+Constraints:
+
+3 <= nums.length <= 104
+-1000 <= nums[i] <= 1000
+
+"""
+
+class Solution:
+    def maximumProduct(self, nums: List[int]) -> int:
+        max1 , max2 , max3 = float("-inf"),float("-inf"),float("-inf")
+        min1 , min2  = float("inf"), float("inf")
+        for num in nums:
+            if num > max1:
+                max3 = max2
+                max2 = max1
+                max1 = num
+            elif num > max2:
+                max3 = max2
+                max2 = num
+            elif num > max3:
+                max3 = num
+            if num < min1:
+                min2 = min1
+                min1 = num
+            elif num < min2:
+                min2 = num
+        return max(min1*min2*max1, max1*max2*max3)
+            
